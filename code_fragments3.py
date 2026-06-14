a1709da1cfdf7bb3354ffeb4f79226e88da63d19
Tsvetan (Цецо) <98406593+TDK74@users.noreply.github.com>
2026-06-14 11:02:41 +0300

    Delete code_fragments3.py from master.

--- code_fragments3.py
+++
@@ -1,9 +0,0 @@
-training_args = TrainingArguments(learning_rate = 2e-6,
-                                weight_decay = 0.01,
-                                per_device_train_batch_size = 20,
-                                per_device_eval_batch_size = 20,
-                                num_train_epochs = 3, )
-## ------------------------------------------------------ ##
-random.seed(seed)
-np.random.seed(seed)
-torch.manual_seed(seed)
